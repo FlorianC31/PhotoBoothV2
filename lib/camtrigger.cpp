4c179e747d0bb1157ef2accfb4afa12a67750862
Florian Champain <f.champain@gmail.com>
2023-02-04 21:25:15 +0100

    CamTrigger class dev (first working version)

--- lib/camtrigger.cpp
+++ lib/camtrigger.cpp
@@ -4,15 +4,26 @@
 #include <codecvt>
 #include <locale>
 
-#define MAX_TEMPO 15
-#define REFRESH_TEMPO 20
+#define MAX_OPEN_TEMPO 10
+#define MAX_LOAD_TEMPO 10
+#define REFRESH_TEMPO 10
+
+#define SIZE_MARGIN 10
+#define MAX_NB_ACTIVATE_TRIES 20
+#define ACTIVATE_TRIES_DURATION 100 // ms
+
+#define KEY_L 0x31
+#define KEY_G 0x47
+#define KEY_1 0x26
+#define KEY_CTRL 0x11
+#define KEY_ENTER 0x0D
 
 CamTrigger::CamTrigger() :
-    m_state(INIT)
+    m_wait(false)
 {
 
-    m_imagingEdgeDesktop = new Window("Imaging Edge Desktop");
-    m_remote = new Window("Remote");
+    m_imagingEdgeDesktop = new IedWindow("Imaging Edge Desktop");
+    m_remote = new RemoteWindow("Remote", m_imagingEdgeDesktop);
 
     while (true){
         loop();
@@ -28,156 +39,57 @@ CamTrigger::~CamTrigger()
 
 void CamTrigger::loop()
 {
-    switch (m_state) {
+    if (m_wait)
+        return;
 
-    case INIT:
-        qDebug() << "State: INIT";
-        if (m_imagingEdgeDesktop->isOpen())
-            openPreRemote();
-        else
-            openIed();
-        break;
-
-    case OPENING_IED:
-        qDebug() << "State: OPENING_IED";
-        if (m_tempo == MAX_TEMPO)
-            error();
-        else if (m_imagingEdgeDesktop->isOpen())
-            openPreRemote();
-        else
-            m_tempo++;
-        break;
-
-    case OPENING_REMOTE:
-        qDebug() << "State: OPENING_REMOTE";
-        if (m_tempo == MAX_TEMPO)
-            error();
-        else if(!m_remote->isOpen())
-            m_tempo++;
-        else if (m_remote->isPreRemote())
-            loadCamera();
-        else if (m_remote->isWarningMsg())
-            refresh();
-        else if (m_remote->isLoading())
-            m_state = CAMERA_LOADING;
-        else
-            m_state = NORMAL_RUN;
-        break;
-
-    case CAMERA_LOADING:
-        qDebug() << "State: CAMERA_LOADING";
-        if (m_tempo == MAX_TEMPO)
-            error();
-        else
-            m_tempo++;
-        break;
-
-    case NORMAL_RUN:
-        qDebug() << "State: NORMAL_RUN";
-        if (m_remote->isWarningMsg())
-            m_state = OPENING_REMOTE;
-        break;
-
-    case REFRESHING:
-        qDebug() << "State: REFRESHING";
-        if (m_tempo == REFRESH_TEMPO)
-            m_state = OPENING_REMOTE;
-        else
-            m_tempo++;
-        break;
-
-    case FAIL:
-        qDebug() << "State: FAIL";
-        break;
+    if (!m_remote->isOpen()) {
+        if (!m_imagingEdgeDesktop->isOpen())
+            m_imagingEdgeDesktop->open();
+        m_remote->open();
     }
-
 }
 
 
-void CamTrigger::error()
-{
-    qDebug() << "Error";
-    m_state = FAIL;
-}
-
-void CamTrigger::refresh()
-{
-    m_state = REFRESHING;
-    m_tempo = 0;
-}
-
-void CamTrigger::loadCamera()
-{
-    m_state = CAMERA_LOADING;
-    m_tempo = 0;
-    m_remote->click(120, 75, true);
-}
-
-
-void CamTrigger::openIed()
-{
-    m_state = OPENING_IED;
-    m_tempo = 0;
-    qDebug() << "Opening Imaging Edge Desktop";
-    std::string imagingPath = "\"C:\\Program Files\\Sony\\Imaging Edge Desktop\\ied.exe\"";
-    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-    std::wstring wfilePath = converter.from_bytes(imagingPath);
-    ShellExecute(NULL, L"open", wfilePath.c_str(), NULL, NULL, SW_SHOWNORMAL);
-    system(imagingPath.c_str());
-}
-
-
-void CamTrigger::openPreRemote()
-{
-    m_state = OPENING_REMOTE;
-    m_tempo = 0;
-    qDebug() << "Opening PreRemote";
-    // Click on "Démarrer" button on remote section
-    m_imagingEdgeDesktop->click(670, 160, false);
-}
-
-
-Window::Window(std::string title) :
-    m_title(title)
-{}
-
+// ###################### Window Class ################################
 
 bool Window::isOpen()
 {
     m_handle = FindWindowA(NULL, m_title.c_str());
-    if (m_handle == NULL) {
-        qDebug() << "The window" << m_title.c_str() << "is not open";
-        return false;
-    }
-    else {
-        qDebug() << "The window" << m_title.c_str() << "is open and it's not a Remote";
-        return true;
-    }
+    return m_handle != NULL;
 }
 
 bool Window::checkSize(int w, int h)
 {
+    if (!isOpen())
+        return false;
     RECT windowRect;
     GetWindowRect(m_handle, &windowRect);
     int windowWidth = windowRect.right - windowRect.left;
     int windowHeight = windowRect.bottom - windowRect.top;
-    return windowWidth == w && windowHeight == h;
+    return abs(windowWidth - w) <= SIZE_MARGIN && abs(windowHeight - h) <= SIZE_MARGIN;
 }
 
+void Window::printSize()
+{
+    if (!isOpen())
+        return;
+    RECT windowRect;
+    GetWindowRect(m_handle, &windowRect);
+    int windowWidth = windowRect.right - windowRect.left;
+    int windowHeight = windowRect.bottom - windowRect.top;
+    qDebug() << "Size of" << m_title.c_str() << ": width =" << windowWidth << "- height =" << windowHeight;
+}
 
 void Window::click(int x, int y, bool doubleClick)
 {
     if (!isOpen())
         return;
 
-    SetForegroundWindow(m_handle);
-    Sleep(10);
-    qDebug() << "Placing focus on" << m_title.c_str();
+    activate();
 
     Point pos = getPos();
 
     SetCursorPos(x + pos.x, y + pos.y);
-    qDebug() << "Clicking in (" << x << "+" << pos.x << "," << y << "+" << pos.y;
 
     mouse_event(MOUSEEVENTF_LEFTDOWN, x + pos.x, y + pos.y, 0, 0);
     mouse_event(MOUSEEVENTF_LEFTUP, x + pos.x, y + pos.y, 0, 0);
@@ -190,6 +102,17 @@ void Window::click(int x, int y, bool doubleClick)
     }
 }
 
+void Window::activate()
+{
+    SetForegroundWindow(m_handle);
+    int nbTries = 0;
+    while (!isActivated() && nbTries < MAX_NB_ACTIVATE_TRIES) {
+        Sleep(ACTIVATE_TRIES_DURATION);
+        nbTries++;
+    }
+    Sleep(500);
+}
+
 
 Point Window::getPos()
 {
@@ -197,5 +120,170 @@ Point Window::getPos()
     GetWindowRect(m_handle, &windowRect);
     Point pos = {windowRect.left, windowRect.top};
     return pos;
+}
+
+void Window::pressKey(BYTE key, BYTE key2) {
+
+    activate();
+
+    keybd_event(key, 0, 0, 0);
+    if (key2 != 0) {
+        Sleep(5);
+        keybd_event(key2, 0, 0, 0);
+    }
+
+    Sleep(200);
+
+    keybd_event(key, 0, KEYEVENTF_KEYUP, 0);
+    if (key2 != 0) {
+        Sleep(5);
+        keybd_event(key2, 0, KEYEVENTF_KEYUP, 0);
+    }
+}
+
+
+// ###################### IedWindow Class ################################
+
+void IedWindow::open()
+{
+    qDebug() << "Open Image Edge Desktop";
+
+    if (isOpen()) {
+        qDebug() << "Image Edge Desktop is already open";
+        return;
+    }
+
+    std::string imagingPath = "\"C:\\Program Files\\Sony\\Imaging Edge Desktop\\ied.exe\"";
+    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+    std::wstring wfilePath = converter.from_bytes(imagingPath);
+    ShellExecute(NULL, L"open", wfilePath.c_str(), NULL, NULL, SW_SHOWNORMAL);
+    system(imagingPath.c_str());
+}
+
+
+
+// ###################### RemoteWindow Class ################################
+
+RemoteWindow::RemoteWindow(std::string title, IedWindow* iedWindow) :
+    Window(title),
+    m_iedWindow(iedWindow)
+{};
+
+
+
+void RemoteWindow::open()
+{
+    m_state = INIT;
+    m_tempo = 0;
+
+    while (m_state != RUNING) {
+
+        switch (m_state){
+        case INIT:
+            qDebug() << "State: INIT";
+            if (!isOpen())
+                openPreRemote();
+            else if (isPreRemote())
+                loadCamera();
+            else if (isWarningMsg())
+                refresh();
+            else if (isLoading())
+                m_state = CAMERA_LOADING;
+            else if (isFinalRemote())
+                m_state = RUNING;
+            else
+                raiseErrorMsg("initializing Remote window");
+            break;
+
+        case OPENING_REMOTE:
+            qDebug() << "State: OPENING_REMOTE";
+            if (isPreRemote())
+                loadCamera();
+            else if (isWarningMsg())
+                refresh();
+            else if (m_tempo == MAX_OPEN_TEMPO)
+                raiseErrorMsg("opening Remote window");
+            else
+                m_tempo++;
+            break;
+
+        case REFRESHING:
+            qDebug() << "State: REFRESHING";
+            if (isPreRemote() && m_tempo == REFRESH_TEMPO)
+                loadCamera();
+            else if (isWarningMsg())
+                refresh();
+            else
+                m_tempo++;
+            break;
+
+        case CAMERA_LOADING:
+            qDebug() << "State: CAMERA_LOADING";
+            if (isFinalRemote())
+                m_state = RUNING;
+            else if (m_tempo == MAX_LOAD_TEMPO)
+                raiseErrorMsg("loading camera");
+            else
+                m_tempo++;
+            break;
+
+        case RUNING:
+            qDebug() << "State: RUNING";
+            break;
+        }
+
+        Sleep(1000);
+    }
+    qDebug() << "State: RUNING";
+    pressKey(KEY_CTRL, KEY_L);
+}
+
+void RemoteWindow::raiseErrorMsg(std::string errorMsg) {
+    qDebug() << "ERROR: Error while" << errorMsg.c_str();
+    m_state = INIT;
+}
+
+
+void RemoteWindow::refresh()
+{
+    qDebug() << "Refresh";
+
+    // Click OK on warning msgBox
+    pressKey(KEY_ENTER);
+
+    int nbTries = 0;
+    while (!isPreRemote() && nbTries < MAX_NB_ACTIVATE_TRIES) {
+        Sleep(ACTIVATE_TRIES_DURATION);
+        nbTries++;
+    }
+
+    click(714, 341, false);
+
+    m_state = REFRESHING;
+    m_tempo = 0;
+}
+
+void RemoteWindow::loadCamera()
+{
+    qDebug() << "Load camera";
+    m_tempo = 0;
+    click(120, 75, true);
+    m_state = CAMERA_LOADING;
+}
+
+
+void RemoteWindow::openPreRemote()
+{
+    m_tempo = 0;
+    qDebug() << "Open PreRemote";
+
+    //m_wait = true;
+    // Click on "Accueil" button
+    m_iedWindow->click(55, 90, false);
+    // Click on "Démarrer" button on remote section
+    m_iedWindow->click(670, 160, false);
+    //m_wait = false;
+
+    m_state = OPENING_REMOTE;
 }
 
--- lib/camtrigger.h
+++ lib/camtrigger.h
@@ -11,27 +11,63 @@ struct Point{
     int y = 0;
 };
 
-
-
 class Window
 {
 private:
 
 public:
-    Window(std::string title);
+    Window(std::string title) : m_title(title) {};
     void click(int x, int y, bool doubleClick);
     bool isOpen();
-    bool isPreRemote() {return checkSize(930, 376);};
-    bool isWarningMsg() {return checkSize(427, 173);};
-    bool isLoading() {return checkSize(427, 173);}; // TODO à modifier
+    void printSize();
+    void show() {activate();};
+    void pressKey(BYTE key, BYTE key2 = 0);
 
-private:
+protected:
     std::string m_title;
     HWND m_handle;
 
-private:
+    bool isActivated() {return GetForegroundWindow() == m_handle;};
+    void activate();
     Point getPos();
     bool checkSize(int w, int h);
+};
+
+
+class IedWindow : public Window
+{
+public:
+    IedWindow(std::string title) : Window(title) {};
+    void open();
+};
+
+
+class RemoteWindow : public Window
+{
+public:
+    RemoteWindow(std::string title, IedWindow* iedWindow);
+    void open();
+
+private:
+    bool isPreRemote() {return checkSize(930, 376);};
+    bool isWarningMsg() {return checkSize(427, 159);};
+    bool isLoading() {return checkSize(541, 245);};
+    bool isFinalRemote() {return isOpen() && !isPreRemote() && !isWarningMsg() && !isLoading();};
+    void loadCamera();
+    void refresh();
+    void openPreRemote();
+    void raiseErrorMsg(std::string errorMsg);
+
+    enum State{
+        INIT,
+        OPENING_REMOTE,
+        REFRESHING,
+        CAMERA_LOADING,
+        RUNING
+    } m_state;
+
+    int m_tempo;
+    IedWindow* m_iedWindow;
 };
 
 
@@ -44,32 +80,12 @@ public:
 
 
 private:
-
-    enum State{
-        INIT,
-        OPENING_IED,
-        OPENING_REMOTE,
-        CAMERA_LOADING,
-        NORMAL_RUN,
-        REFRESHING,
-        FAIL
-    };
-
-    // State machine actions
-    void openIed();
-    void openPreRemote();
-    void loadCamera();
-    void refresh();
-
-    bool isOpen(HWND window) {return window == NULL;};
-    void error();
     void loop();
 
-    Window* m_imagingEdgeDesktop;
-    Window* m_remote;
-
-    State m_state;
+    IedWindow* m_imagingEdgeDesktop;
+    RemoteWindow* m_remote;
     int m_tempo;
+    bool m_wait;
 
 };
 
